62faf5be74f7fcfed9faf5a64e24e7321427ee69
Jarod Castillo <43973221+Smotto@users.noreply.github.com>
2025-06-16 14:01:13 -0700

    fixed dependencies, removed assimp for now clarified README.md

--- README.md
+++ README.md
@@ -8,16 +8,16 @@
       ```
       C:\Program Files\Microsoft Visual Studio\2022\Community\VC\vcpkg
       ```
-
+   * [Ninja](https://github.com/ninja-build/ninja/wiki/Pre-built-Ninja-packages) should already come with the bundled visual studio 2022 toolchain in CLion. If you want to manually do it, set PATH to ```ninja.exe``` location.
 2. Install [Vulkan SDK](https://vulkan.lunarg.com/) associated with your OS. Just tick every optional box, you have
-   enough storage.
-3. You might need to install [Ninja](https://github.com/ninja-build/ninja/wiki/Pre-built-Ninja-packages) associated with your OS.
+   enough storage. Check if it's in PATH.
 
 ### Building Pantomir - CLion and VSCode
 
 1. CMakePresets does a lot of the work for you. You can open this project up in CLion or VSCode with the Cmake
    extension.
 2. Make sure to set the **working directory** to the root of the project, so it can touch the Assets folder.
+3. Enable the profiles I've made from CMakePresets.json for CLion.
 
 ### Notes and Concerns
 
--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -6,9 +6,8 @@ endif()
 set(CMAKE_TOOLCHAIN_FILE "$ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake" CACHE STRING "vcpkg toolchain file")
 
 # All files in the Dependencies folder need to be included.
-include(${CMAKE_CURRENT_LIST_DIR}/Dependencies/imgui.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/Dependencies/assimp.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/Dependencies/SDL3.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/Dependencies/imgui.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/Dependencies/Stb.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/Dependencies/glm.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/Dependencies/xxHash.cmake)
@@ -18,9 +17,8 @@ include(${CMAKE_CURRENT_LIST_DIR}/Dependencies/vk-bootstrap.cmake)
 find_package(Vulkan REQUIRED) # Vulkan INSTALLED ON YOUR OS
 
 # All vcpkgs need to be found for CMake
-find_package(glm CONFIG REQUIRED)
 find_package(SDL3 CONFIG REQUIRED)
-find_package(assimp CONFIG REQUIRED)
+find_package(glm CONFIG REQUIRED)
 find_package(Stb REQUIRED) # Might want to use just the image part for Stb
 find_package(xxHash CONFIG REQUIRED)
 find_package(VulkanMemoryAllocator CONFIG REQUIRED)
